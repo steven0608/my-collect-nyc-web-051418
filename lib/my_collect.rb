267b008ac14d59f608a3b2d0c83b8434a4266aee
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 22:28:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array)
+  new_array=[]
 for i in array
   if i.include? (" ")
-    i.split(" ").first
+    new_array << i.split(" ").first
   else
-    i.upcase
+    new_array << i.upcase
   end
 end
+new_array
 end
